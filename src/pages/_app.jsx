240e2bd0162d7194fd70e5f8bb4c04a25226db4b
SoloPython <alexander.eric.3@gmail.com>
2023-04-11 19:47:08 -0500

    APP is the SEO Issue

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,22 +1,5 @@
 import '@/styles/globals.css';
-import '@/styles/toastStyles.css';
-import '@/styles/customVideo.css';
-
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { store, persistor } from '@/redux/store';
 
 export default function App({ Component, pageProps }) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page);
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {/* <ThemeProvider enableSystem attribute="class"> */}
-        {getLayout(<Component {...pageProps} />)}
-        {/* <ToastContainer className="bottom-0" position="bottom-right" /> */}
-        {/* </ThemeProvider> */}
-      </PersistGate>
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }

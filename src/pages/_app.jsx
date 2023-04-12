be0b91b572a34ab98e51e712d63285fb804355b2
SoloPython <alexander.eric.3@gmail.com>
2023-04-11 20:00:18 -0500

    APP is the SEO Issue, testing With REdux

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,18 @@
 import '@/styles/globals.css';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from '@/redux/store';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {/* <ThemeProvider enableSystem attribute="class"> */}
+        <Component {...pageProps} />
+        {/* <ToastContainer className="bottom-0" position="bottom-right" /> */}
+        {/* </ThemeProvider> */}
+      </PersistGate>
+    </Provider>
+  );
 }

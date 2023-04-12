ba4b32e2e3c92a647238009cfd239bcadd61bd5a
SoloPython <alexander.eric.3@gmail.com>
2023-04-12 12:19:12 -0500

    GPDR cookies

--- src/hocs/Layout.jsx
+++ src/hocs/Layout.jsx
@@ -1,58 +1,35 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { acceptCookies, rejectCookies } from '../redux/actions/cookies/cookies';
-import ScrollToTop from './components/ScrollToTop';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 import Link from 'next/link';
+import ScrollToTop from './components/ScrollToTop';
 import Footer from '@/features/footer';
 import Navbar from '@/features/navbar';
-import {
-  checkAuthenticated,
-  getUserDelivery,
-  loadEthereumBalance,
-  loadGalrBalance,
-  loadMaticPolygonBalance,
-  loadPraediumBalance,
-  loadUser,
-  loadUserProfile,
-  loadUserWallet,
-  refresh,
-} from '@/redux/actions/auth/auth';
-import { useEffect } from 'react';
-import { getCartTotal, getItems } from '@/redux/actions/cart/cart';
 
 export default function Layout({ children }) {
-  const dispatch = useDispatch();
-  const cookies = useSelector((state) => state.cookies.cookies);
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  const cartItems = useSelector((state) => state.cart.items);
-  const wallet = useSelector((state) => state.auth.wallet);
-
+  const [cookiesAccepted, setCookiesAccepted] = useState(true);
   useEffect(() => {
-    if (isAuthenticated) {
-      // dispatch(refresh());
-      // dispatch(checkAuthenticated());
-      dispatch(loadUser());
-      // dispatch(loadUserProfile());
-      // dispatch(loadUserWallet()).then(() => {
-      //   dispatch(loadEthereumBalance(wallet && wallet.address));
-      //   dispatch(loadPraediumBalance(wallet && wallet.polygon_address));
-      //   dispatch(loadGalrBalance(wallet && wallet.polygon_address));
-      //   dispatch(loadMaticPolygonBalance(wallet && wallet.polygon_address));
-      // });
-      // dispatch(getItems());
+    const gdprCookieValue = Cookies.get('gdpr');
+    if (gdprCookieValue !== 'accepted') {
+      setCookiesAccepted(false);
     }
-    // eslint-disable-next-line
-  }, [isAuthenticated, dispatch]);
+  }, []);
 
-  useEffect(() => {
-    dispatch(getCartTotal(cartItems));
-  }, [dispatch, cartItems]);
+  const acceptCookies = () => {
+    Cookies.set('gdpr', 'accepted', { expires: 365 });
+    setCookiesAccepted(true);
+  };
+
+  const rejectCookies = () => {
+    Cookies.set('gdpr', 'rejected', { expires: 365 });
+    setCookiesAccepted(false);
+  };
 
   return (
     <>
       <ScrollToTop />
       <Navbar />
       {children}
-      {cookies === null && (
+      {cookiesAccepted === false && (
         <div className="pointer-events-none fixed inset-x-0 bottom-0 px-6 pb-6">
           <div className="pointer-events-auto ml-auto max-w-xl rounded-xl bg-white dark:bg-dark-second p-6 shadow-lg dark:ring-gray-900/10">
             <p className="text-sm leading-6 text-gray-900 dark:text-dark-txt">
@@ -68,7 +45,7 @@ export default function Layout({ children }) {
               <button
                 type="button"
                 onClick={() => {
-                  dispatch(acceptCookies());
+                  acceptCookies();
                 }}
                 className="rounded-md bg-dark-main px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 dark:hover:bg-dark-third focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
               >
@@ -77,7 +54,7 @@ export default function Layout({ children }) {
               <button
                 type="button"
                 onClick={() => {
-                  dispatch(rejectCookies());
+                  rejectCookies();
                 }}
                 className="text-sm font-semibold leading-6 text-gray-900 dark:text-dark-txt"
               >
